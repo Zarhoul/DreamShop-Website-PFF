5d2761d2cc855e1678565df71928f9921ff0c8cc
Zarhoul <zarhoul21@gmail.com>
2023-01-23 21:15:24 +0100

    Create and Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FirstBar from './FirstBar'
+import SecondBar from './SecondBar'
+import NavBar from './NavBar'
+
+function Header() {
+  return (
+    <>
+        <FirstBar />
+        <SecondBar />
+        <NavBar />
+    </>
+  )
+}
+
+export default Header
